f6d918e39facad1645ba71a88239ad2203b6ac41
emhelp <sparrkuss@gmail.com>
2026-04-04 11:35:21 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'emhelp-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/templates.html',
+  '/status.html',
+  '/calculators.html',
+  '/consilium.html',
+  '/prikaz.html',
+  '/stations.html',
+  '/icon-512.png',
+  '/1712743647196.png'
+];
+
+// Установка сервис-воркера и кэширование файлов
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Перехват запросов: сначала пытаемся из сети, если нет - из кэша
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Если запрос успешен, клонируем и сохраняем в кэш
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+// Обновление: удаляем старый кэш, когда активируется новый воркер
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
